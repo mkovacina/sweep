2b5d1a1305ea60c57e492d1f2a799b50c64b258c
mike kovacina <michael.kovacina@gmail.com>
2020-01-10 10:52:44 +0000

    giving the error method a variadic nature

--- src/errors.c
+++ src/errors.c
@@ -1,24 +1,27 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 extern FILE* ERROR_FILE;
 
-void start_log() {
-
+void start_log() 
+{
   ERROR_FILE = fopen("error.log", "w");
-
 }
 
-void error(char* error_message) {
+void error(const char* fmt, ...) 
+{
+  va_list args;
+  va_start(args, fmt);
+    
+  fprintf(ERROR_FILE, fmt, args);
+  fprintf(stdout, fmt, args);
 
-  fprintf(ERROR_FILE, "%s", error_message);
-  fprintf(stdout, "%s\n", error_message);
-
+  va_end(args);
 }
 
-void stop_log() {
-
+void stop_log() 
+{
   fclose(ERROR_FILE);
-
 }
 
   
