cb3bdcc68b7270b8936563ed7468ee3cfc7e09dd
mike kovacina <michael.kovacina@gmail.com>
2020-01-16 20:58:55 +0000

    forgotten carriage returns

--- src/tests-fsa.c
+++ src/tests-fsa.c
@@ -13,15 +13,15 @@ void test_initialize_fsa_simple()
     FILE* tmpFsaFile = tmpfile();
 
     // sources from all-follow.fsa
-    fprintf(tmpFsaFile, "1  number of fsa's in the file");
-    fprintf(tmpFsaFile, "#  Leader FSA comes first");
-    fprintf(tmpFsaFile, "2  number of states in fsa");
-    fprintf(tmpFsaFile, "#");
-    fprintf(tmpFsaFile, "2  number of input symbols");
-    fprintf(tmpFsaFile, "#");
-    fprintf(tmpFsaFile, "0-0:0;1:1-wander");
-    fprintf(tmpFsaFile, "1-0:0;1:1-follow");
-    fprintf(tmpFsaFile, "####");
+    fprintf(tmpFsaFile, "1  number of fsa's in the file\n");
+    fprintf(tmpFsaFile, "#  Leader FSA comes first\n");
+    fprintf(tmpFsaFile, "2  number of states in fsa\n");
+    fprintf(tmpFsaFile, "#\n");
+    fprintf(tmpFsaFile, "2  number of input symbols\n");
+    fprintf(tmpFsaFile, "#\n");
+    fprintf(tmpFsaFile, "0-0:0;1:1-wander\n");
+    fprintf(tmpFsaFile, "1-0:0;1:1-follow\n");
+    fprintf(tmpFsaFile, "####\n");
     rewind(tmpFsaFile);
 
     fsa_table_struct fsaTable;
@@ -29,6 +29,24 @@ void test_initialize_fsa_simple()
 
     assert( status == SUCCESS && "failed to initialize the FSA table");
     assert( fsaTable.number_fsa == 1 && "incorrect number of FSAs found");
+
+    fsa_struct *fsa = &(fsaTable.fsa[0]);
+
+printf("%d\n", fsaTable.fsa[0].current_state);
+    assert( fsa->current_state == 0 && "incorrect current FSA state" );
+    assert( fsa->number_states == 2 && "incorrect number of FSA states");
+
+
+    state_struct* state0 = &fsa->state[0];
+    state_struct* state1 = &fsa->state[1];
+
+    assert( state0->number_transitions == 2 && "incorrect number of transitions");
+    assert( strncmp(state0->description,"wander",6) && "incorrect state name");
+
+    //transition_struct* transition_0_0 = state1->transition[0];
+    //transition_struct* transition_0_1 = state1->transition[1];
+
+    //assert( transition_0_0->input)
 }
 
 
