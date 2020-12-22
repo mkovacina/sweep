91c8886fbd5b68ff2bdbcd28f36e2aa8fce73846
mike kovacina <michael.kovacina@gmail.com>
2020-12-22 12:30:24 +0000

    fixing the inner conditional to write to the correct locations

--- exps/life/life.c
+++ exps/life/life.c
@@ -1,28 +1,34 @@
 void set_neighborhood(agent_struct *agent, int action_index)
-/*  Generates a firefly "flash" within a limited area of the grid */
+	/*  Generates a firefly "flash" within a limited area of the grid */
 {
-  /* acquire the position of the current agent */
-  int Xloc = agent->x_pos;
-  int Yloc = agent->y_pos;
-   
-  /* acquire the FSA state of the current agent */
-  int state = agent->fsa->current_state;
+	/* acquire the position of the current agent */
+	int Xloc = agent->x_pos;
+	int Yloc = agent->y_pos;
 
-  /* determines which support grid contains pertinent information */
-  /* by accessing the support grid to be updated by this function */
-  int action_grid = agent->state_functions[state].function_list[action_index].update_grids[0].grid;
+	/* acquire the FSA state of the current agent */
+	int state = agent->fsa->current_state;
 
-  int rows = GGETR(0);
-  int cols = GGETC(0);
+	/* determines which support grid contains pertinent information */
+	/* by accessing the support grid to be updated by this function */
+	int action_grid = agent->state_functions[state].function_list[action_index].update_grids[0].grid;
 
-  for (int row = Yloc-1; row < Yloc+2; row++)
-  {
-    for  (int col = Xloc-1; col < Xloc+2; col++)
+	int rows = GGETR(0);
+	int cols = GGETC(0);
+
+	TraceDebug("rows = %d, cols = %d", rows, cols);
+
+	for (int row = Yloc-1; row < Yloc+2; row++)
 	{
-      if (row >= 0 && row < rows && col >= 0 && col < cols && (row != Yloc || col != Xloc))
-	  {
-		  GSET_CUM(action_grid, row, col, 1);
-      }
-    }
-  }
+		for  (int col = Xloc-1; col < Xloc+2; col++)
+		{
+			if (row >= 0 && row < rows && col >= 0 && col < cols && !(row == Yloc && col == Xloc))
+			{
+				int oldValue = GCURR(action_grid, row, col);
+				GSET_CUM(action_grid, row, col, 1);
+				int newValue = GCURR(action_grid, row, col);
+				// don't forget the grids are FLOATS, not ints
+				TraceDebug("[r,c]=[%d,%d]  %d => %d", row, col, oldValue, newValue);
+			}
+		}
+	}
 }
