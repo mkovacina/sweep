87be3571e4e87de88038160cab4be0000d96cfb5
mike kovacina <michael.kovacina@gmail.com>
2020-12-23 12:53:10 +0000

    fixing up this header file

--- src/constants.h
+++ src/constants.h
@@ -1,16 +1,7 @@
-#ifndef _CONSTANTS_INCLUDED_
-#define _CONSTANTS_INCLUDED_
+#pragma once
 #define FAILURE_PERCENTAGE 0.0
 #define CORRECT_FILE_NUMBER 5
 #define MAX_BUFFER 256
 #define MAX_FIELDS 21
 #define INIT_CHEM_AMOUNT 495
 #define INJECT_DECREASE 1
-//#define VERBOSE 
-
-#include <stdio.h>
-typedef FILE * fptr;
-#endif /* _CONSTANTS_INCLUDED_  nothing goes below this line */
-
-
-
